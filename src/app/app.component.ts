e8e76d6f546b18be4a90d428f11f8b3507436540
Mohamad J Makki <mohamad.j.makki@gmail.com>
2023-12-11 21:12:07 +0300

    Luhn JS

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import {CardValidationComponent} from "./comp/layer/card-validation/card-validation.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, CardValidationComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
